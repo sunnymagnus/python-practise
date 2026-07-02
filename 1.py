879cc42675c334d5d12709c1138eb672f3292e48
sunnymagnus <adarshvardhankanchi11224@gmail.com>
2026-07-02 15:15:36 +0530

    basic printing programs

---
+++ 1.py
@@ -0,0 +1,42 @@
+#10 typing exercises 
+"""
+1.Print text
+2.Print multiple lines
+3.Print blank lines
+4.Print using \n
+5.Print quotes
+6.Print patterns using only print()
+
+"""
+#1
+print("adarsh-pardhu")
+
+#2
+print("\n\n")
+print("adarsh")
+print("pardhu")
+print("venu")
+
+#3
+print("\n\n")
+print("helloww")
+print()
+print("worldd!!!")
+
+#4
+print("\n\n")
+print("hellow \npardhu")
+
+#5
+print("\n\n")
+print("\"adarsh\"")
+
+#6
+print("\n\n")
+print("  ********")
+print("  ********")
+print("  **      ")
+print("  ********")
+print("  **      ")
+print("  ********")
+print("  ********")
