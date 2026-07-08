cf13a2e372b66257193deebb22580a51c7efd9dc
sunnymagnus <adarshvardhankanchi11224@gmail.com>
2026-07-08 20:26:59 +0530

    rank finder

---
+++ IfElse/RankFinder.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter the marks: "))
+
+if marks>=80 :
+    print("you got rank 'A'")
+elif marks>=60 :
+    print("you got rank 'B'")
+elif marks>=40 :
+    print("you got rank 'C'")
+else :
+    print("you FAILED!!")
