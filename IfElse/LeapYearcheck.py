a0bf71662bdcfbec087bfa27053ac555ee01f046
sunnymagnus <adarshvardhankanchi11224@gmail.com>
2026-07-08 20:36:43 +0530

    leap year finder

--- IfElse/LeapYearcheck.py
+++ IfElse/LeapYearcheck.py
@@ -1,6 +1,6 @@
 year = int(input("Enter year: "))
 
-if year % 4 == 0 and year % 100 != 0 :
+if year % 4 == 0 and year % 100 != 0  or year % 400 == 0 :
     print("entered year is A leap year")
 else:
     print("It is not a leap year")
