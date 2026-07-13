0f22c5ed8f860c5fc55a9a4c81404daa302825a9
sunnymagnus <adarshvardhankanchi11224@gmail.com>
2026-07-13 21:52:34 +0530

    Find reverse of a number

---
+++ Loops/ReverseAnumber.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a Number to reverse it :"))
+
+r=int(0)
+n=int(num)
+rev=int(0)
+
+while n>0 :
+    r=n%10
+    n=n//10
+    rev=rev*10+r
+
+print(f"The reverse of {num} is {rev}")
+
