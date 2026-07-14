19103f59529257bc6742fe8d7bf05db1f4646a5a
sunnymagnus <adarshvardhankanchi11224@gmail.com>
2026-07-14 21:18:20 +0530

    example on creating a list

---
+++ Lists/ExOnLists.py
@@ -0,0 +1,9 @@
+# list of integer type
+nums = [1,2,3,4]
+print(nums)
+# list of string type
+names = ["adarsh","pardhu","venu"]
+print(names)
+# list of mixed data type
+ele = [12,"mix",3.12,True]
+print(ele)
